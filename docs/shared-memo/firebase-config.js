a6e08d5978cd088d51e40512018416d5ab798b8e
maruma <maruyama_001@jimusuru.info>
2026-08-12 10:13:42 +0900

    Update firebase-config.js

--- docs/shared-memo/firebase-config.js
+++ docs/shared-memo/firebase-config.js
@@ -1,11 +1,11 @@
 // Firebase Console の「プロジェクトの設定 > マイアプリ」で取得した値に置き換えてください。
 export const firebaseConfig = {
-  apiKey: "Firebase Consoleに表示された値",
+  apiKey: "AIzaSyBWZNQ0WMlamg7RvnVFhhFzt_Mc8cU3QnU",
   authDomain: "shared-memo-63202.firebaseapp.com",
   projectId: "shared-memo-63202",
   storageBucket: "shared-memo-63202.firebasestorage.app",
   messagingSenderId: "366609342732",
-  appId: "Firebase Consoleに表示された値",
+  appId: "1:366609342732:web:71e06f618bcff36c3d2929"
 };
 
 // V1で利用するグループ。将来はユーザーが選択したグループIDに置き換えられます。
