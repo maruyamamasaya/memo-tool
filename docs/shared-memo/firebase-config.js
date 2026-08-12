81d2972780e29f3075065aa7708da84f07aca1de
maruma <maruyama_001@jimusuru.info>
2026-08-12 10:10:05 +0900

    Finalize shared memo Firebase configuration

--- docs/shared-memo/firebase-config.js
+++ docs/shared-memo/firebase-config.js
@@ -1,11 +1,11 @@
 // Firebase Console の「プロジェクトの設定 > マイアプリ」で取得した値に置き換えてください。
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "Firebase Consoleに表示された値",
+  authDomain: "shared-memo-63202.firebaseapp.com",
+  projectId: "shared-memo-63202",
+  storageBucket: "shared-memo-63202.firebasestorage.app",
+  messagingSenderId: "366609342732",
+  appId: "Firebase Consoleに表示された値",
 };
 
 // V1で利用するグループ。将来はユーザーが選択したグループIDに置き換えられます。
